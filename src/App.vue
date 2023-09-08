52679d273a91cd53152e2440de9f6d0b6395cf91
devkevinpak <kevin.pak@obigo.com>
2023-09-08 11:08:41 +0900

    라우터테스트

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  testsetasetawstasetawsetawsetgwetawet
+  <div id="app">
+    <h1>Vue Router Example</h1>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  font-size: 2em;
+  margin-bottom: 20px;
+}
+
+a {
+  margin: 10px;
+  font-size: 1.2em;
 }
 </style>
